d6577e520e8751cd8530108181c708c80050ab92
David Henry <dhenry@dragonspears.com>
2016-10-31 17:29:57 -0500

    nullable for type change

--- DragonMail.DTO/DSMail.cs
+++ DragonMail.DTO/DSMail.cs
@@ -19,7 +19,7 @@ namespace DragonMail.DTO
         public string Subject { get; set; }
         public string TextBody { get; set; }
         public string HtmlBody { get; set; }
-        public DateTime SentDate { get; set; }
+        public DateTime? SentDate { get; set; }
         public string Queue { get; set; }
         public string LogMessage()
         {
