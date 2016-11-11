edb69601300fb765095903a3ade03625a0034d82
David Henry <dhenry@dragonspears.com>
2016-11-10 22:17:16 -0600

    ui updates ( file size, message count)

---
+++ DragonMail.Web/Common/FileSizeHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DragonMail.Web
+{
+    public static class FileSizeHelper
+    {
+        private static readonly string[] Units = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+       
+        public static string GetReadableFileSize(this int size) // Size in bytes
+        {
+            int unitIndex = 0;
+            while (size >= 1024)
+            {
+                size /= 1024;
+                ++unitIndex;
+            }
+
+            string unit = Units[unitIndex];
+            return string.Format("{0:0.#} {1}", size, unit);
+        }
+    }
+}
